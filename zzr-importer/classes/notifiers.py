31fc854ca5fa9cc8b738899eda13e6af46aeed27
alexmisk <alexmisk@ya.ru>
2020-09-04 20:14:02 +0300

    refactor: better internal channels handling

--- zzr-importer/classes/notifiers.py
+++ zzr-importer/classes/notifiers.py
@@ -4,11 +4,10 @@ from classes.notification_channels import NotificationChannel
 class Notifier:
     def __init__(self):
         super().__init__()
-        self._notification_channels = []
+        self._notification_channels = set()
 
     def add_notification_channel(self, channel: NotificationChannel):
-        if not (channel in self._notification_channels):
-            self._notification_channels.append(channel)
+        self._notification_channels.add(channel)
 
     def notify(self):
         [channel.notify() for channel in self._notification_channels]
